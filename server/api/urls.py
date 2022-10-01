73b2788d5410e513626a873ef5b8a05c612266b7
Alec Xiang <alec@actively.ai>
2022-10-01 16:42:36 -0400

    fix urls

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,13 +1,23 @@
-from django.urls import path 
-from . import views
+from django.urls import path
 
+from .views import login, order, populate, postorder, profile, restaurants
 
 urlpatterns = [
-    path('ping/', views.ping),
-    path('pre_populate/', views.pre_populate),
-    path('login/', views.login), 
-    path('signup/', views.signup),
-    path('restaurant/<str>:id',views.get_restaurant),
-    path('menus/',views.get_menus_for_restaurant),
-    path('foods/',views.get_foods_for_menu),
+    path("ping/", populate.ping),
+    path("pre_populate/", populate.pre_populate),
+    
+    path("login/", login.login),
+    path("signup/", login.signup),
+
+    path("restaurant/<str>:id", restaurants.get_restaurant),
+    path("menus/", restaurants.get_menus_for_restaurant),
+
+    path("foods/", order.get_foods_for_menu),
+    path("order/", order.order),
+
+    path("ratings/", postorder.post_ratings),
+    path("review/", postorder.post_review),
+
+    path("orderhistory/", profile.check_order_history),
+    path("tasteprofile/", profile.get_taste_profile),
 ]
