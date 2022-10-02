ea9c642b3c4098bd7f9c78b5dd7701242a256031
Alec Xiang <alec@actively.ai>
2022-10-02 02:45:04 -0400

    fix

--- server/ml/utils.py
+++ server/ml/utils.py
@@ -1,4 +1,4 @@
-from api.models import Food, FoodOrder, UserOrder
+from api.models import FoodOrder, UserOrder
 
 
 def retrieve_user_food_history(user_id):
@@ -9,9 +9,9 @@ def retrieve_user_food_history(user_id):
         return user_food_history
 
     for user_order in user_orders:
-        food_orders = FoodOrder.objects.get(user_order_id=user_order.id)
+        food_orders = list(FoodOrder.objects.all().filter(user_order_id=user_order.id))
         for food_order in food_orders:
-            food = Food.objects.get(id=food_order.food_id)
-            user_food_history.append((food.name, food.ingredients, food_order.rating))
+            food = food_order.food_id
+            user_food_history.append((food.name, ", ".join(food.ingredients), food_order.rating))
 
     return user_food_history
